db900ee6b10b62ff9a5e3bb632cf2e40c5d46312
zhengliqian <66684196+zhengliqian@users.noreply.github.com>
2025-12-08 18:24:50 +0800

    Add files via upload

---
+++ alpg_ctrl_core/alpg_ctrl_core.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company               : 
+// Engineer              : 
+// 
+// Create Date           : 2025-06-25
+// Module Name           : alpg_ctrl_core
+// Project Name          : 
+// Target Devices        : 
+// Tool Versions         : Vivado 2020.2
+// Description           : 
+// 
+// Dependencies          : 
+// 
+// Revision              :
+//                        Revision v0.01 - File Created
+// Additional Comments   :
+// 
+//////////////////////////////////////////////////////////////////////////////////
+module alpg_ctrl_core 
+#(
+    
+) 
+(
+    input                           clk           ,
+    input                           rst           
+);
+    
+endmodule
